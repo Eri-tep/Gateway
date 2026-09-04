76635fe10b35353761816fc7cff2ace0401c54db
Eri <jithy@icloud.com>
2026-09-04 14:51:44 +0900

    v1.1.2

--- include/Common.h
+++ include/Common.h
@@ -168,7 +168,7 @@ namespace TimeUtils {
 
 namespace Config {
 // [시스템] 펌웨어 버전 문자열 (CLI/Log/OTA)
-constexpr const char *FIRMWARE_VERSION = "v1.1.1";
+constexpr const char *FIRMWARE_VERSION = "v1.1.2";
 } // namespace Config
 
 namespace Config::Task {
